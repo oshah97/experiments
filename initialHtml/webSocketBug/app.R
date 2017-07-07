a7b82fa2a5a4abccccfe63bb2e0478a988383944
oshah97 <oshah97@outlook.com>
2017-07-07 13:53:42 -0700

    addition of working demo and bug

---
+++ initialHtml/webSocketBug/app.R
@@ -0,0 +1,64 @@
+library(httpuv)
+library(jsonlite)
+
+myws <- NULL
+
+.lastMessage <- NULL
+#--------------------------------------------------------------------------------
+setup <- function(wsCon)
+{
+   wsCon$open <- FALSE
+   wsCon$wsID <- NULL
+   wsCon$ws <- NULL
+   wsCon$result <- NULL
+
+   wsCon$call = function(req) { # "call" processes http requests
+     wsUrl = paste(sep='',
+                   '"',
+                  "ws://",
+                  ifelse(is.null(req$HTTP_HOST), req$SERVER_NAME, req$HTTP_HOST),
+                  '"')
+    list(
+      status = 200L,
+      headers = list('Content-Type' = 'text/html'),
+      body = c(file="index.html"))
+     }
+   wsCon$onWSOpen = function(ws) {
+      wsCon$ws <- ws
+      ws$onMessage(function(binary, rawMessage) {
+
+          print(rawMessage)
+
+         }) # onMessage
+       wsCon$open <- TRUE
+       } # onWSOpen
+
+   wsCon
+
+} # setup
+#--------------------------------------------------------------------------------
+my.send <- function(wsCon, msg)
+{
+  #  browser()
+  wsCon$ws$send(toJSON(msg))
+
+} # send
+#--------------------------------------------------------------------------------
+wsCon <- new.env(parent=emptyenv())
+#--------------------------------------------------------------------------------
+demo <- function()
+{
+   wsCon <- setup(wsCon)
+   port <- 8543
+   browseURL(sprintf("http://localhost:%d", port))
+   wsCon$id <- startDaemonizedServer("0.0.0.0", port, wsCon)
+
+   Sys.sleep(2)
+   my.send(wsCon, "test1")
+   Sys.sleep(2)
+
+   my.send(wsCon, "WebSocket Works!")
+   Sys.sleep(2)
+
+} # demo
+#--------------------------------------------------------------------------------
