cf08e3b3e71dcbdb0a0eca24138730c3117f726d
oshah97 <oshah97@outlook.com>
2017-07-24 11:26:02 -0700

    tests True if data frame, matrix & list

--- MyPackage/inst/unitTests/test_dataFrame.R
+++ MyPackage/inst/unitTests/test_dataFrame.R
@@ -1,3 +1,6 @@
 test_dataFrame <- function() {
-    checkTrue(is.list(dataFrame()))
+    checkTrue(is.list(dataFrameRaw))
+    checkTrue(is.matrix(mat))
+    checkTrue(is.data.frame(col_nodes))
+    checkTrue(is.data.frame(row_nodes))
 }  
