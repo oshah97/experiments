a1f6a8d039323d8f75cca7244cacc4d5bb18b149
pricelab <pricelab@porch.systemsbiology.net>
2017-07-27 15:30:18 -0700

    addition of smaller torture test and package version unit tests

--- initialHtml/webSocketBug/app.R
+++ initialHtml/webSocketBug/app.R
@@ -1,5 +1,6 @@
 library(httpuv)
 library(jsonlite)
+library(RUnit)
 #--------------------------------------------------------------------------------
 .lastMessage <- NULL;
 #--------------------------------------------------------------------------------
@@ -45,15 +46,14 @@ my.send <- function(wsCon, msg)
 init <- function()
 {
    wsCon <- configureWebSocketServer()
-   port <- 8665
+   port <- 8682
    browseURL(sprintf("http://localhost:%d", port))
    wsCon$id <- startDaemonizedServer("0.0.0.0", port, wsCon)
    return(wsCon)
 
 } # init
 #--------------------------------------------------------------------------------
-demo <- function(wsCon)
-{
+demo <- function(wsCon) {
    printf("about to send test1 to browser for capitalization")
    my.send(wsCon, "test1")
    Sys.sleep(2)
@@ -107,7 +107,7 @@ toMatrixPrint <- function(wsCon, mat=matrix(
                                      c(1,2,3,4,5,6,7,8,9),
                                      nrow=3,
                                      ncol=3,
-                                     byrow=TRUE)) {   
+                                     byrow=TRUE)) {
     .lastMessage <<- NULL
     msg <- list(cmd="toMatrixPrint",
                 callback="handleResult",
@@ -115,16 +115,16 @@ toMatrixPrint <- function(wsCon, mat=matrix(
     my.send(wsCon, msg)
     while(is.null(.lastMessage)) {
           Sys.sleep(0.1)
-          }     
+          }
     return(.lastMessage$payload)
     }#toMatrixPrint
 #--------------------------------------------------------------------------------
 tortureTest <- function() {
 
-    printf("=== test sends differnt sized matrices through a websocket and prints time is takes to receive the return message")
-    printf("=== tests eight matrices; final matrix is 500 x 1000; results print below matrix size text")
-    printf("=== test estimated to take between 10 - 15 minutes")
-    
+    print("=== test sends differnt sized matrices through a websocket and prints time is takes to receive the return message")
+    print("=== tests eight matrices; final matrix is 500 x 1000; results print below matrix size text")
+    print("=== test estimated to take between 10 - 15 minutes")
+
     mat <<- matrix(1:100, nrow=10)
     print("=== 10x10 matrix")
     print(system.time((mat2 <<- toMatrixPrint(wsCon, mat))))
@@ -136,7 +136,8 @@ tortureTest <- function() {
     mat <<- matrix(1:40000, nrow=200)
     print("=== 200x200 matrix")
     print(system.time((mat2 <<- toMatrixPrint(wsCon, mat))))
-    
+
+
     mat <<- matrix(1:90000, nrow=300)
     print("=== 300x300 matrix")
     print(system.time((mat2 <<- toMatrixPrint(wsCon, mat))))
@@ -144,7 +145,7 @@ tortureTest <- function() {
     mat <<- matrix(1:160000, nrow=400)
     print("=== 400x400 matrix")
     print(system.time((mat2 <<- toMatrixPrint(wsCon, mat))))
-    
+
     mat <<- matrix(1:250000, nrow=500)
     print("=== 500x500 matrix")
     print(system.time((mat2 <<- toMatrixPrint(wsCon, mat))))
@@ -156,12 +157,25 @@ tortureTest <- function() {
     mat <<- matrix(1:500000, nrow=1000)
     print("=== 500x1000 matrix")
     print(system.time((mat2 <<- toMatrixPrint(wsCon, mat))))
-   
+
     }#TortureTest
+#--------------------------------------------------------------------------------
+tortureTestSmall <- function() {
+    print("=== test 100 x 100 matrix")
+    mat <<- matrix(1:10000, nrow=100)
+    print(system.time(mat2 <<- toMatrixPrint(wsCon, mat)))
+
+}#tortureTestSmall
 #--------------------------------------------------------------------------------
 handleResult <- function(message) {
     printf("---handleResult")
     .lastMessage <<- message$payload
     NULL
     }
+#--------------------------------------------------------------------------------
+test_packageVersion <- function() {
+    checkTrue(packageVersion("RUnit") >= "0.4.31")
+    checkTrue(packageVersion("jsonlite") >= "1.5")
+    checkTrue(packageVersion("httpuv") >= "1.3.5")
+    }#test_packageVersion
 #--------------------------------------------------------------------------------
